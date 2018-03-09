2e1bff86c4bd9918dac25e2c6815b19b996d3054
Vitaly Rotari <cky.adc@gmail.com>
2018-03-09 18:40:38 +0200

    Fix android build error

--- android/src/main/java/com/greatdroid/reactnative/media/MediaKitPackage.java
+++ android/src/main/java/com/greatdroid/reactnative/media/MediaKitPackage.java
@@ -13,18 +13,15 @@ import java.util.List;
 
 public class MediaKitPackage implements ReactPackage {
 
-  @Override
   public List<NativeModule> createNativeModules(ReactApplicationContext reactContext) {
     return Collections.emptyList();
   }
 
 
-  @Override
   public List<Class<? extends JavaScriptModule>> createJSModules() {
     return Collections.emptyList();
   }
 
-  @Override
   public List<ViewManager> createViewManagers(ReactApplicationContext reactContext) {
     return Arrays.<ViewManager>asList(new ReactMediaPlayerViewManager());
   }
